4941e0472fc66048e34cfd7f08f7127697ae7f07
Anita Ilieva <anita.ilieva24@gmail.com>
2017-03-08 13:04:57 +0100

    Converting the application to use _id

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { MongoClient } from 'mongodb';
+import { MongoClient, ObjectID } from 'mongodb';
 import assert from 'assert';
 import config from '../config';
 
@@ -28,15 +28,15 @@ router.get('/contests', (req, res) => {
                 return;
             }
 
-            contests[contest.id] = contest;
+            contests[contest._id] = contest;
         });
 });
 
 router.get('/names/:nameIds', (req, res) => {
-    const nameIds = req.params.nameIds.split(',').map(Number);
+    const nameIds = req.params.nameIds.split(',').map(ObjectID);
     let names = {};
     setTimeout(function () {
-        mdb.collection('names').find({ id: { $in: nameIds }})
+        mdb.collection('names').find({ _id: { $in: nameIds }})
             .each((err, name) => {
                 assert.equal(null, err);
 
@@ -45,7 +45,7 @@ router.get('/names/:nameIds', (req, res) => {
                     return;
                 }
 
-                names[name.id] = name;
+                names[name._id] = name;
             });
     }, 4000);
 });
@@ -53,7 +53,7 @@ router.get('/names/:nameIds', (req, res) => {
 
 router.get('/contests/:contestId', (req, res) => {
     mdb.collection('contests')
-        .findOne({ id: Number(req.params.contestId) })
+        .findOne({ _id: ObjectID(req.params.contestId) })
         .then(contest => res.send(contest))
         .catch(console.error);
 });
